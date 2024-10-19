d4957691c75d21f86ad6940dd3e74b404500e99a
abhishek170602 <abhitechy17@gmail.com>
2024-10-19 09:09:47 +0530

    name corrected2

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Nav from "@/components/Home/Nav";
 import { ClerkProvider } from "@clerk/nextjs";
 import Footer from "@/components/Home/Footer";
-import StoreProvider from "@/StoreProvider/storeProvider";
+import StoreProvider from "@/StoreProvider/StoreProvider";
 import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
